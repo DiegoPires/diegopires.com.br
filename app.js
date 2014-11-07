f484477fd4bb8c2ed79e3b937d563cf5d9b25681
Diego Silva Pires <diego_silva_pires@outlook.com>
2014-11-07 15:48:57 -0200

    Add translations and 404 page

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var i18n = require('i18next');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
+//var errors = require('./routes/error');
+
+i18n.init({
+    saveMissing: true,
+    debug: true,
+    fallbackLng : 'pt'
+});
 
 var app = express();
 
@@ -16,21 +24,53 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+app.use(i18n.handle);
+//app.use(favicon()); 
+//app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.enable('verbose errors');
+
+// disable them in production
+// use $ NODE_ENV=production node examples/error-pages
+if ('production' == app.settings.env) app.disable('verbose errors');
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
+//app.use('/error', errors);
+
+app.get('/404', function(req, res, next){
+  // trigger a 404 since no other middleware
+  // will match /404 after this one, and we're not
+  // responding here
+  next();
+});
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    //var err = new Error('Not Found');
+    //err.status = 404;
+    //next(err);
+
+  // respond with HTML
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+
 });
 
 /// error handlers
@@ -57,5 +97,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
+i18n.registerAppHelper(app);
 
 module.exports = app;
